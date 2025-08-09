13b93f46fde239c119f08afc1fc40045524eeac4
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-08-09 11:31:16 +0300

    Update color resources and themes for Material Design 3 integration

--- app/src/main/java/com/koniukhov/cinecircle/MainActivity.kt
+++ app/src/main/java/com/koniukhov/cinecircle/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.koniukhov.cinecircle
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.koniukhov.cinecircle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,14 +20,6 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
