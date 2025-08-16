fb6e60c23a29b41348eaead88ceb24f07e8a7d3a
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-08-16 14:24:59 +0300

    Add ViewPager and TabLayout for Movies and TV Series navigation

--- feature/home/src/main/kotlin/com/koniukhov/cinecircle/feature/home/HomeFragment.kt
+++ feature/home/src/main/kotlin/com/koniukhov/cinecircle/feature/home/HomeFragment.kt
@@ -5,31 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.faltenreich.skeletonlayout.Skeleton
-import com.faltenreich.skeletonlayout.applySkeleton
-import com.koniukhov.cinecircle.feature.home.adapter.GenreUiAdapter
-import com.koniukhov.cinecircle.feature.home.adapter.MoviesAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import com.koniukhov.cinecircle.feature.home.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
-
-    private val viewModel: HomeViewModel by viewModels()
-
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-
-    private lateinit var trendingSkeleton: Skeleton
-    private lateinit var mowPlayingSkeleton: Skeleton
-    private lateinit var popularSkeleton: Skeleton
-    private lateinit var topRatedSkeleton: Skeleton
-    private lateinit var upcomingSkeleton: Skeleton
-    private lateinit var genreSkeleton: Skeleton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,128 +23,43 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        initAllRecyclerViews()
-        initAllRecyclerSkeletons()
-        showAllSkeletons()
-
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        lifecycleScope.launch {
-            viewModel.loadMoviesForAllCategories()
-            viewModel.uiState.collect {
-                if (!it.isLoading && it.error == null){
-                    hideAllSkeletons()
-                    setDataToRecyclers(it)
+        setupViewPagerAdapter()
+        setupTabMediator()
+    }
+
+    private fun setupViewPagerAdapter(){
+        binding.viewPager.adapter = createAdapter()
+    }
+
+    private fun createAdapter(): FragmentStateAdapter {
+        return object : FragmentStateAdapter(this) {
+            override fun getItemCount() = 2
+            override fun createFragment(position: Int): Fragment {
+                return when (position) {
+                    0 -> MoviesHomeFragment()
+                    1 -> MoviesHomeFragment()
+                    else -> throw IllegalStateException()
                 }
             }
         }
     }
 
+    private fun setupTabMediator() {
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> getString(R.string.movies_title)
+                1 -> getString(R.string.tv_series_title)
+                else -> ""
+            }
+        }.attach()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-    private fun initAllRecyclerViews() {
-        initTrendingRecyclerView()
-        initNowPlayingRecyclerView()
-        initPopularRecyclerView()
-        initTopRatedRecyclerView()
-        initUpcomingRecyclerView()
-        initMoviesGenreRecyclerView()
-    }
-    private fun initTrendingRecyclerView() {
-        binding.trendingRecyclerView.layoutManager = LinearLayoutManager(
-            requireContext(),
-            LinearLayoutManager.HORIZONTAL,
-            false
-        )
-        binding.trendingRecyclerView.adapter = MoviesAdapter(){
-        }
-    }
-    private fun initNowPlayingRecyclerView() {
-        binding.nowPlayingRecyclerView.layoutManager = LinearLayoutManager(
-            requireContext(),
-            LinearLayoutManager.HORIZONTAL,
-            false
-        )
-        binding.nowPlayingRecyclerView.adapter = MoviesAdapter(){
-        }
-
-    }
-    private fun initPopularRecyclerView() {
-        binding.popularRecyclerView.layoutManager = LinearLayoutManager(
-            requireContext(),
-            LinearLayoutManager.HORIZONTAL,
-            false
-        )
-        binding.popularRecyclerView.adapter = MoviesAdapter(){
-        }
-
-    }
-    private fun initTopRatedRecyclerView() {
-        binding.topRatedRecyclerView.layoutManager = LinearLayoutManager(
-            requireContext(),
-            LinearLayoutManager.HORIZONTAL,
-            false
-        )
-        binding.topRatedRecyclerView.adapter = MoviesAdapter(){
-        }
-    }
-    private fun initUpcomingRecyclerView() {
-        binding.upcomingRecyclerView.layoutManager = LinearLayoutManager(
-            requireContext(),
-            LinearLayoutManager.HORIZONTAL,
-            false
-        )
-        binding.upcomingRecyclerView.adapter = MoviesAdapter(){
-        }
-    }
-    private fun initMoviesGenreRecyclerView() {
-        binding.genreRecyclerView.layoutManager = LinearLayoutManager(
-            requireContext(),
-            LinearLayoutManager.HORIZONTAL,
-            false
-        )
-        binding.genreRecyclerView.adapter = GenreUiAdapter(){
-        }
-    }
-    private fun initAllRecyclerSkeletons() {
-        trendingSkeleton = binding.trendingRecyclerView.applySkeleton(R.layout.item_home_movie, RECYCLER_SKELETON_ITEM_COUNT)
-        mowPlayingSkeleton = binding.nowPlayingRecyclerView.applySkeleton(R.layout.item_home_movie, RECYCLER_SKELETON_ITEM_COUNT)
-        popularSkeleton = binding.popularRecyclerView.applySkeleton(R.layout.item_home_movie, RECYCLER_SKELETON_ITEM_COUNT)
-        topRatedSkeleton = binding.topRatedRecyclerView.applySkeleton(R.layout.item_home_movie, RECYCLER_SKELETON_ITEM_COUNT)
-        upcomingSkeleton = binding.upcomingRecyclerView.applySkeleton(R.layout.item_home_movie, RECYCLER_SKELETON_ITEM_COUNT)
-        genreSkeleton = binding.genreRecyclerView.applySkeleton(R.layout.item_home_genre_ui, RECYCLER_SKELETON_ITEM_COUNT)
-    }
-    private fun showAllSkeletons() {
-        trendingSkeleton.showSkeleton()
-        mowPlayingSkeleton.showSkeleton()
-        popularSkeleton.showSkeleton()
-        topRatedSkeleton.showSkeleton()
-        upcomingSkeleton.showSkeleton()
-        genreSkeleton.showSkeleton()
-    }
-    private fun hideAllSkeletons() {
-        trendingSkeleton.showOriginal()
-        mowPlayingSkeleton.showOriginal()
-        popularSkeleton.showOriginal()
-        topRatedSkeleton.showOriginal()
-        upcomingSkeleton.showOriginal()
-        genreSkeleton.showOriginal()
-    }
-    private fun setDataToRecyclers(ui: MoviesUiState){
-        (binding.trendingRecyclerView.adapter as? MoviesAdapter)?.setData(ui.trendingMovies)
-        (binding.nowPlayingRecyclerView.adapter as? MoviesAdapter)?.setData(ui.nowPlayingMovies)
-        (binding.popularRecyclerView.adapter as? MoviesAdapter)?.setData(ui.popularMovies)
-        (binding.topRatedRecyclerView.adapter as? MoviesAdapter)?.setData(ui.topRatedMovies)
-        (binding.upcomingRecyclerView.adapter as? MoviesAdapter)?.setData(ui.upcomingMovies)
-        (binding.genreRecyclerView.adapter as? GenreUiAdapter)?.setData(ui.genreUiMovies)
-    }
-
-    companion object{
-        const val RECYCLER_SKELETON_ITEM_COUNT = 5
     }
 }
---
+++ feature/home/src/main/kotlin/com/koniukhov/cinecircle/feature/home/MoviesHomeFragment.kt
@@ -0,0 +1,167 @@
+package com.koniukhov.cinecircle.feature.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.faltenreich.skeletonlayout.Skeleton
+import com.faltenreich.skeletonlayout.applySkeleton
+import com.koniukhov.cinecircle.feature.home.adapter.GenreUiAdapter
+import com.koniukhov.cinecircle.feature.home.adapter.MoviesAdapter
+import com.koniukhov.cinecircle.feature.home.databinding.FragmentMoviesHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import kotlin.getValue
+
+@AndroidEntryPoint
+class MoviesHomeFragment : Fragment() {
+    private val viewModel: HomeViewModel by viewModels()
+
+    private var _binding: FragmentMoviesHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var trendingSkeleton: Skeleton
+    private lateinit var mowPlayingSkeleton: Skeleton
+    private lateinit var popularSkeleton: Skeleton
+    private lateinit var topRatedSkeleton: Skeleton
+    private lateinit var upcomingSkeleton: Skeleton
+    private lateinit var genreSkeleton: Skeleton
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMoviesHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        initAllRecyclerViews()
+        initAllRecyclerSkeletons()
+        showAllSkeletons()
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            viewModel.loadMoviesForAllCategories()
+            viewModel.uiState.collect {
+                if (!it.isLoading && it.error == null){
+                    hideAllSkeletons()
+                    setDataToRecyclers(it)
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initAllRecyclerViews() {
+        initTrendingRecyclerView()
+        initNowPlayingRecyclerView()
+        initPopularRecyclerView()
+        initTopRatedRecyclerView()
+        initUpcomingRecyclerView()
+        initMoviesGenreRecyclerView()
+    }
+    private fun initTrendingRecyclerView() {
+        binding.trendingRecyclerView.layoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        binding.trendingRecyclerView.adapter = MoviesAdapter(){
+        }
+    }
+    private fun initNowPlayingRecyclerView() {
+        binding.nowPlayingRecyclerView.layoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        binding.nowPlayingRecyclerView.adapter = MoviesAdapter(){
+        }
+
+    }
+    private fun initPopularRecyclerView() {
+        binding.popularRecyclerView.layoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        binding.popularRecyclerView.adapter = MoviesAdapter(){
+        }
+
+    }
+    private fun initTopRatedRecyclerView() {
+        binding.topRatedRecyclerView.layoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        binding.topRatedRecyclerView.adapter = MoviesAdapter(){
+        }
+    }
+    private fun initUpcomingRecyclerView() {
+        binding.upcomingRecyclerView.layoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        binding.upcomingRecyclerView.adapter = MoviesAdapter(){
+        }
+    }
+    private fun initMoviesGenreRecyclerView() {
+        binding.genreRecyclerView.layoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        binding.genreRecyclerView.adapter = GenreUiAdapter(){
+        }
+    }
+    private fun initAllRecyclerSkeletons() {
+        trendingSkeleton = binding.trendingRecyclerView.applySkeleton(R.layout.item_home_movie, RECYCLER_SKELETON_ITEM_COUNT)
+        mowPlayingSkeleton = binding.nowPlayingRecyclerView.applySkeleton(R.layout.item_home_movie, RECYCLER_SKELETON_ITEM_COUNT)
+        popularSkeleton = binding.popularRecyclerView.applySkeleton(R.layout.item_home_movie, RECYCLER_SKELETON_ITEM_COUNT)
+        topRatedSkeleton = binding.topRatedRecyclerView.applySkeleton(R.layout.item_home_movie, RECYCLER_SKELETON_ITEM_COUNT)
+        upcomingSkeleton = binding.upcomingRecyclerView.applySkeleton(R.layout.item_home_movie, RECYCLER_SKELETON_ITEM_COUNT)
+        genreSkeleton = binding.genreRecyclerView.applySkeleton(R.layout.item_home_genre_ui, RECYCLER_SKELETON_ITEM_COUNT)
+    }
+    private fun showAllSkeletons() {
+        trendingSkeleton.showSkeleton()
+        mowPlayingSkeleton.showSkeleton()
+        popularSkeleton.showSkeleton()
+        topRatedSkeleton.showSkeleton()
+        upcomingSkeleton.showSkeleton()
+        genreSkeleton.showSkeleton()
+    }
+    private fun hideAllSkeletons() {
+        trendingSkeleton.showOriginal()
+        mowPlayingSkeleton.showOriginal()
+        popularSkeleton.showOriginal()
+        topRatedSkeleton.showOriginal()
+        upcomingSkeleton.showOriginal()
+        genreSkeleton.showOriginal()
+    }
+    private fun setDataToRecyclers(ui: MoviesUiState){
+        (binding.trendingRecyclerView.adapter as? MoviesAdapter)?.setData(ui.trendingMovies)
+        (binding.nowPlayingRecyclerView.adapter as? MoviesAdapter)?.setData(ui.nowPlayingMovies)
+        (binding.popularRecyclerView.adapter as? MoviesAdapter)?.setData(ui.popularMovies)
+        (binding.topRatedRecyclerView.adapter as? MoviesAdapter)?.setData(ui.topRatedMovies)
+        (binding.upcomingRecyclerView.adapter as? MoviesAdapter)?.setData(ui.upcomingMovies)
+        (binding.genreRecyclerView.adapter as? GenreUiAdapter)?.setData(ui.genreUiMovies)
+    }
+
+    companion object{
+        const val RECYCLER_SKELETON_ITEM_COUNT = 5
+    }
+}
