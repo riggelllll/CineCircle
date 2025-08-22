adcbd56b6e48b950936d02ef967f20ad3df13f97
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-08-22 11:57:02 +0300

    Implement equals and hashCode methods in MediaItem class

--- core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/model/MediaItem.kt
+++ core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/model/MediaItem.kt
@@ -2,4 +2,16 @@ package com.koniukhov.cinecircle.core.domain.model
 
 sealed class MediaItem {
     abstract val id: Int
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MediaItem
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id
+    }
 }
