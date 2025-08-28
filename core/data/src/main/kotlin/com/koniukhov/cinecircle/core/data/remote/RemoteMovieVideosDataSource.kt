af9bcba8043d85703a061d3599465faecbec03fc
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-08-28 20:30:33 +0300

    Add RemoteMovieVideosDataSource for fetching movie videos and Video data class

---
+++ core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/remote/RemoteMovieVideosDataSource.kt
@@ -0,0 +1,16 @@
+package com.koniukhov.cinecircle.core.data.remote
+
+import com.koniukhov.cinecircle.core.data.BuildConfig
+import com.koniukhov.cinecircle.core.domain.datasource.VideosDataSource
+import com.koniukhov.cinecircle.core.network.api.TMDBApi
+import com.koniukhov.cinecircle.core.network.model.MovieVideosDto
+import javax.inject.Inject
+
+class RemoteMovieVideosDataSource @Inject constructor(private val api: TMDBApi) : VideosDataSource {
+    override suspend fun getMovieVideos(
+        movieId: Int,
+        language: String
+    ): MovieVideosDto {
+        return api.getMovieVideos(movieId, BuildConfig.API_KEY, language).body() ?: MovieVideosDto.empty()
+    }
+}
---
+++ core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/model/Video.kt
@@ -0,0 +1,14 @@
+package com.koniukhov.cinecircle.core.domain.model
+
+data class Video(
+    val languageCode: String,
+    val countryCode: String,
+    val name: String,
+    val key: String,
+    val site: String,
+    val size: Int,
+    val type: String,
+    val official: Boolean,
+    val publishedAt: String,
+    val id: String
+)
