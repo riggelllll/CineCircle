59a4117136a60f619245c9880fa58160480a19b7
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-08-28 11:51:22 +0300

    Implement ImagesRepository with remote data source integration for fetching movie images

---
+++ core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/repository/ImagesRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.koniukhov.cinecircle.core.data.repository
+
+import com.koniukhov.cinecircle.core.data.mapper.toDomain
+import com.koniukhov.cinecircle.core.data.remote.RemoteImagesDataSource
+import com.koniukhov.cinecircle.core.domain.model.MediaImages
+import com.koniukhov.cinecircle.core.domain.repository.ImagesRepository
+import javax.inject.Inject
+
+class ImagesRepositoryImpl @Inject constructor(private val remoteImagesDataSource: RemoteImagesDataSource): ImagesRepository{
+    override suspend fun getMovieImages(
+        movieId: Int,
+        language: String
+    ): MediaImages {
+        val dto = remoteImagesDataSource.getMovieImages(movieId, language)
+        return dto.toDomain()
+    }
+}
