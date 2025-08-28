28fa8fb36edbd6e3552c6ff06df61906c8ef23d7
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-08-28 11:39:58 +0300

    Add remote data source for fetching movie images

---
+++ core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/remote/RemoteImagesDataSource.kt
@@ -0,0 +1,16 @@
+package com.koniukhov.cinecircle.core.data.remote
+
+import com.koniukhov.cinecircle.core.data.BuildConfig
+import com.koniukhov.cinecircle.core.domain.datasource.ImagesDataSource
+import com.koniukhov.cinecircle.core.network.api.TMDBApi
+import com.koniukhov.cinecircle.core.network.model.MediaImagesDto
+import javax.inject.Inject
+
+class RemoteImagesDataSource @Inject constructor(private val api: TMDBApi): ImagesDataSource {
+    override suspend fun getMovieImages(
+        movieId: Int,
+        language: String
+    ): MediaImagesDto {
+        return api.getMovieImages(movieId, BuildConfig.API_KEY, language).body() ?: MediaImagesDto.empty()
+    }
+}
