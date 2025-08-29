d7f756ba1d5b1bb9cf0ef33ba207531c670a067f
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-08-29 15:47:26 +0300

    Add GetMovieRecommendations use case to fetch movie recommendations

---
+++ core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/usecase/GetMovieRecommendationsUseCase.kt
@@ -0,0 +1,11 @@
+package com.koniukhov.cinecircle.core.domain.usecase
+
+import com.koniukhov.cinecircle.core.domain.repository.MoviesRepository
+import javax.inject.Inject
+
+class GetMovieRecommendationsUseCase @Inject constructor(
+    private val repository: MoviesRepository
+) {
+    suspend operator fun invoke(movieId: Int, page: Int, language: String) =
+        repository.getMovieRecommendations(movieId, page, language)
+}
