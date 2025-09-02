484c5efc857115aa172e818410f44459d4b4ad8a
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-02 15:59:30 +0300

    Add ReleaseType enum to define various release formats

---
+++ core/common/src/main/kotlin/com/koniukhov/cinecircle/core/common/model/ReleaseType.kt
@@ -0,0 +1,16 @@
+package com.koniukhov.cinecircle.core.common.model
+
+enum class ReleaseType(val value: Int) {
+    PREMIERE(1),
+    THEATRICAL_LIMITED(2),
+    THEATRICAL(3),
+    DIGITAL(4),
+    PHYSICAL(5),
+    TV(6);
+
+    companion object {
+        fun fromValue(value: Int): ReleaseType? {
+            return values().find { it.value == value }
+        }
+    }
+}
