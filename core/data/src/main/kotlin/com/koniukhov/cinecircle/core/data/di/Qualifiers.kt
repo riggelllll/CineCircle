8f72cb3be7c1c1564c01bdad8d61ade161c8abda
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-03 19:21:16 +0300

    Add CountryCode and LanguageCode qualifiers for dependency injection

---
+++ core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/di/Qualifiers.kt
@@ -0,0 +1,11 @@
+package com.koniukhov.cinecircle.core.data.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class CountryCode
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class LanguageCode
