b806d2154bd53d73bcdd181882ebf8a32b2d681e
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-08 16:35:25 +0300

    Add method to filter YouTube trailers in MovieVideos data class

--- core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/model/MovieVideos.kt
+++ core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/model/MovieVideos.kt
@@ -1,6 +1,16 @@
 package com.koniukhov.cinecircle.core.domain.model
 
+import com.koniukhov.cinecircle.core.common.Constants.VIDEO_SITE_YOUTUBE
+import com.koniukhov.cinecircle.core.common.Constants.VIDEO_TYPE_TRAILER
+
 data class MovieVideos(
     val id: Int,
     val results: List<Video>
-)
+){
+    fun getYouTubeTrailers(): List<Video> {
+        return results.filter {
+            it.site.equals(VIDEO_SITE_YOUTUBE, ignoreCase = true) &&
+            it.type.equals(VIDEO_TYPE_TRAILER, ignoreCase = true)
+        }
+    }
+}
