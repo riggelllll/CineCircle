f12eeff0a1dcd9ef0fb1e08e13efd5dbea0b5667
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-10 19:53:00 +0300

    Add getTvSeriesVideos function to VideosRepository and implement in VideosRepositoryImpl

--- core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/repository/VideosRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/repository/VideosRepositoryImpl.kt
@@ -1,17 +1,25 @@
 package com.koniukhov.cinecircle.core.data.repository
 
 import com.koniukhov.cinecircle.core.data.mapper.toDomain
-import com.koniukhov.cinecircle.core.data.remote.RemoteMovieVideosDataSource
+import com.koniukhov.cinecircle.core.data.remote.RemoteVideosDataSource
 import com.koniukhov.cinecircle.core.domain.model.MovieVideos
 import com.koniukhov.cinecircle.core.domain.repository.VideosRepository
 import javax.inject.Inject
 
-class VideosRepositoryImpl @Inject constructor(private val remoteImagesDataSource: RemoteMovieVideosDataSource) : VideosRepository{
+class VideosRepositoryImpl @Inject constructor(private val remoteVideosDataSource: RemoteVideosDataSource) : VideosRepository{
     override suspend fun getMovieVideos(
         movieId: Int,
         language: String
     ): MovieVideos {
-        val dto = remoteImagesDataSource.getMovieVideos(movieId, language)
+        val dto = remoteVideosDataSource.getMovieVideos(movieId, language)
+        return dto.toDomain()
+    }
+
+    override suspend fun getTvSeriesVideos(
+        tvSeriesId: Int,
+        language: String
+    ): MovieVideos {
+        val dto = remoteVideosDataSource.getTvSeriesVideos(tvSeriesId, language)
         return dto.toDomain()
     }
 }
--- core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/repository/VideosRepository.kt
+++ core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/repository/VideosRepository.kt
@@ -4,4 +4,5 @@ import com.koniukhov.cinecircle.core.domain.model.MovieVideos
 
 interface VideosRepository {
     suspend fun getMovieVideos(movieId: Int, language: String): MovieVideos
+    suspend fun getTvSeriesVideos(tvSeriesId: Int, language: String): MovieVideos
 }
