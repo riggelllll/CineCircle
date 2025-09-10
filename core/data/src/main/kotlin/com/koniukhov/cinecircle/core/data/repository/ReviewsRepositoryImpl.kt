4a5e816604529917e2520bb7331d8631d94bb542
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-10 20:02:35 +0300

    Add getTvSeriesReviews method to ReviewsRepository and implement in ReviewsRepositoryImpl

--- core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/repository/ReviewsRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/repository/ReviewsRepositoryImpl.kt
@@ -1,18 +1,27 @@
 package com.koniukhov.cinecircle.core.data.repository
 
 import com.koniukhov.cinecircle.core.data.mapper.toDomain
-import com.koniukhov.cinecircle.core.data.remote.RemoteMovieReviewsDataSource
+import com.koniukhov.cinecircle.core.data.remote.RemoteReviewsDataSource
 import com.koniukhov.cinecircle.core.domain.model.MovieReview
 import com.koniukhov.cinecircle.core.domain.repository.ReviewsRepository
 import javax.inject.Inject
 
-class ReviewsRepositoryImpl @Inject constructor(private val remoteMovieReviewsDataSource: RemoteMovieReviewsDataSource) : ReviewsRepository {
+class ReviewsRepositoryImpl @Inject constructor(private val remoteReviewsDataSource: RemoteReviewsDataSource) : ReviewsRepository {
     override suspend fun getMovieReviews(
         movieId: Int,
         page: Int,
         language: String
     ): List<MovieReview> {
-        val tdo = remoteMovieReviewsDataSource.getMovieReviews(movieId, page, language)
+        val tdo = remoteReviewsDataSource.getMovieReviews(movieId, page, language)
+        return tdo.results.map { it.toDomain() }
+    }
+
+    override suspend fun getTvSeriesReviews(
+        tvSeriesId: Int,
+        page: Int,
+        language: String
+    ): List<MovieReview> {
+        val tdo = remoteReviewsDataSource.getTvSeriesReviews(tvSeriesId, page, language)
         return tdo.results.map { it.toDomain() }
     }
 }
--- core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/repository/ReviewsRepository.kt
+++ core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/repository/ReviewsRepository.kt
@@ -4,4 +4,5 @@ import com.koniukhov.cinecircle.core.domain.model.MovieReview
 
 interface ReviewsRepository {
     suspend fun getMovieReviews(movieId: Int, page: Int, language: String): List<MovieReview>
+    suspend fun getTvSeriesReviews(tvSeriesId: Int, page: Int, language: String): List<MovieReview>
 }
