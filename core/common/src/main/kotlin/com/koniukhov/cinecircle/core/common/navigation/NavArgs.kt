85c54f33fc88a66cd8383001a93a56d3f482d105
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-10 16:08:44 +0300

    Add TV series details URI function to NavArgs for deep link support

--- core/common/src/main/kotlin/com/koniukhov/cinecircle/core/common/navigation/NavArgs.kt
+++ core/common/src/main/kotlin/com/koniukhov/cinecircle/core/common/navigation/NavArgs.kt
@@ -9,10 +9,14 @@ object NavArgs {
     const val ARG_TITLE = "title"
     const val ARG_GENRE_ID = "genre_id"
     const val ARG_MOVIE_ID = "movie_id"
+    const val ARG_TV_SERIES_ID = "tv_series_id"
 
     fun mediaListUri(type: MediaListType, encodedTitle: String, genreId: Int): Uri =
         "app://cinecircle/mediaList?$ARG_TYPE=$type&$ARG_TITLE=${encodedTitle}&${ARG_GENRE_ID}=${genreId}".toUri()
 
     fun movieDetailsUri(movieId: Int): Uri =
         "app://cinecircle/movie_details?$ARG_MOVIE_ID=$movieId".toUri()
+
+    fun tvSeriesDetailsUri(tvSeriesId: Int): Uri =
+        "app://cinecircle/tv_series_details?$ARG_TV_SERIES_ID=$tvSeriesId".toUri()
 }
