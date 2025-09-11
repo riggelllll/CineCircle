abcfe6794fdb7480f385fb8cf6974eb90aa31389
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-11 10:23:12 +0300

    Add ContentRatingsResponseDto for handling content ratings response

---
+++ core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/ContentRatingsResponseDto.kt
@@ -0,0 +1,12 @@
+package com.koniukhov.cinecircle.core.network.model
+
+import com.koniukhov.cinecircle.core.common.Constants.INVALID_ID
+
+data class ContentRatingsResponseDto(
+    val id: Int,
+    val results: List<ContentRatingDto>
+){
+    companion object{
+        fun empty() = ContentRatingsResponseDto(INVALID_ID, emptyList())
+    }
+}
