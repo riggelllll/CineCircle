15fccc576c3b9e28fe19fdcdac86317b103477ec
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-13 09:09:24 +0300

    Add EpisodesBottomSheetDialog to display episodes for a selected season

---
+++ feature/media_details/src/main/kotlin/com/koniukhov/cinecircle/feature/media/details/dialog/EpisodesBottomSheetDialog.kt
@@ -0,0 +1,71 @@
+package com.koniukhov.cinecircle.feature.media.details.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.koniukhov.cinecircle.core.domain.model.TvSeasonDetails
+import com.koniukhov.cinecircle.feature.media.details.adapter.EpisodeAdapter
+import com.koniukhov.cinecircle.feature.movie_details.R
+import com.koniukhov.cinecircle.feature.movie_details.databinding.DialogEpisodesBottomSheetBinding
+
+class EpisodesBottomSheetDialog : BottomSheetDialogFragment() {
+
+    private var _binding: DialogEpisodesBottomSheetBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var episodeAdapter: EpisodeAdapter
+    private var seasonDetails: TvSeasonDetails? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogEpisodesBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        setupData()
+    }
+
+    private fun setupRecyclerView() {
+        episodeAdapter = EpisodeAdapter()
+        binding.recyclerEpisodes.adapter = episodeAdapter
+    }
+
+    private fun setupData() {
+        seasonDetails?.let { season ->
+            with(binding) {
+                seasonTitle.text = season.name
+                episodesCount.text = getString(
+                    R.string.episodes_count,
+                    season.episodes.size
+                )
+                episodeAdapter.setEpisodes(season.episodes)
+            }
+        }
+    }
+
+    fun setSeasonDetails(season: TvSeasonDetails) {
+        this.seasonDetails = season
+        if (_binding != null) {
+            setupData()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        fun newInstance(): EpisodesBottomSheetDialog {
+            return EpisodesBottomSheetDialog()
+        }
+    }
+}
