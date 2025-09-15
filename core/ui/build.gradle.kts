d4a6bfdc7342dfcfaa38797d5645c713c8429d5e
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-15 10:58:59 +0300

    Add build configuration for CineCircle core UI module

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    alias(libs.plugins.cinecircle.android.library)
+}
+
+android {
+    namespace = "com.koniukhov.cinecircle.core.ui"
+}
+
+dependencies{
+    implementation(libs.androidx.core.ktx)
+    implementation(projects.core.design)
+    implementation(projects.core.domain)
+    implementation(projects.core.common)
+    implementation(projects.core.network)
+    implementation(libs.bundles.coil)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.material)
+}
