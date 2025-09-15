86badd2777054093ec1f7f51c18f2306adca0eb2
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-15 20:06:20 +0300

    Update MovieVideos to include teaser videos in YouTube results

--- core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/model/MovieVideos.kt
+++ core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/model/MovieVideos.kt
@@ -1,16 +1,18 @@
 package com.koniukhov.cinecircle.core.domain.model
 
 import com.koniukhov.cinecircle.core.common.Constants.VIDEO_SITE_YOUTUBE
+import com.koniukhov.cinecircle.core.common.Constants.VIDEO_TYPE_TEASER
 import com.koniukhov.cinecircle.core.common.Constants.VIDEO_TYPE_TRAILER
 
 data class MovieVideos(
     val id: Int,
     val results: List<Video>
 ){
-    fun getYouTubeTrailers(): List<Video> {
+    fun getYouTubeTrailersAndTeasers(): List<Video> {
         return results.filter {
             it.site.equals(VIDEO_SITE_YOUTUBE, ignoreCase = true) &&
-            it.type.equals(VIDEO_TYPE_TRAILER, ignoreCase = true)
-        }
+            (it.type.equals(VIDEO_TYPE_TRAILER, ignoreCase = true) ||
+            it.type.equals(VIDEO_TYPE_TEASER, ignoreCase = true))
+        }.sortedBy { it.type != VIDEO_TYPE_TRAILER }
     }
 }
