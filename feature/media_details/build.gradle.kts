542c5a2fd5e61f6602398d274820f93eb2e4267a
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-15 17:31:32 +0300

    Add database module dependency in build.gradle.kts

--- feature/media_details/build.gradle.kts
+++ feature/media_details/build.gradle.kts
@@ -10,6 +10,7 @@ android {
 dependencies{
     implementation(projects.core.domain)
     implementation(projects.core.data)
+    implementation(projects.core.database)
     implementation(projects.core.design)
     implementation(projects.core.network)
     implementation(projects.core.common)
