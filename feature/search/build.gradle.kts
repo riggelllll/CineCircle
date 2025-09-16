2c3c1733159e296c9240eebd0efab554a21be242
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-16 15:53:07 +0300

    Add Hilt plugin and dependencies for search feature

--- feature/search/build.gradle.kts
+++ feature/search/build.gradle.kts
@@ -1,7 +1,22 @@
 plugins {
     alias(libs.plugins.cinecircle.android.feature)
+    alias(libs.plugins.cinecircle.hilt)
 }
 
 android {
     namespace = "com.koniukhov.cinecircle.feature.search"
+}
+dependencies {
+    implementation(projects.core.domain)
+    implementation(projects.core.data)
+    implementation(projects.core.design)
+    implementation(projects.core.network)
+    implementation(projects.core.common)
+    implementation(projects.core.ui)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.material)
+    implementation(libs.bundles.coil)
+    implementation(libs.skeletonlayout)
 }
