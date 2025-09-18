0cd71a62cf67d92647b8b6eb5769ad0b14f3bbad
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-18 11:17:57 +0300

    Add GetSearchedMoviesUseCase to handle movie search logic

---
+++ core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/usecase/GetSearchedMoviesUseCase.kt
@@ -0,0 +1,9 @@
+package com.koniukhov.cinecircle.core.domain.usecase
+
+import com.koniukhov.cinecircle.core.domain.repository.MoviesRepository
+import javax.inject.Inject
+
+class GetSearchedMoviesUseCase @Inject constructor(private val repository: MoviesRepository) {
+    suspend operator fun invoke(query: String, page: Int, language: String) =
+        repository.getSearchedMovies(query, page, language)
+}
