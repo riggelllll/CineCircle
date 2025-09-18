254149ebf3489ba46fb68e22901440f9b7abbaad
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-18 11:26:02 +0300

    Add getSearchedTvSeriesUseCase to SearchViewModel for TV series data loading

--- feature/search/src/main/kotlin/com/koniukhov/cinecircle/feature/search/viewmodel/SearchViewModel.kt
+++ feature/search/src/main/kotlin/com/koniukhov/cinecircle/feature/search/viewmodel/SearchViewModel.kt
@@ -1,9 +1,11 @@
 package com.koniukhov.cinecircle.feature.search.viewmodel
 
+import android.R.attr.data
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.koniukhov.cinecircle.core.data.di.LanguageCode
 import com.koniukhov.cinecircle.core.domain.usecase.GetSearchedMoviesUseCase
+import com.koniukhov.cinecircle.core.domain.usecase.GetSearchedTvSeriesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -12,13 +14,15 @@ import javax.inject.Inject
 @HiltViewModel
 class SearchViewModel @Inject constructor(
     private val getSearchedMoviesUseCase: GetSearchedMoviesUseCase,
+    private val getSearchedTvSeriesUseCase: GetSearchedTvSeriesUseCase,
     @LanguageCode
     private val languageCode: String
 ) : ViewModel() {
     fun loadData(){
         viewModelScope.launch {
-            val data = getSearchedMoviesUseCase("Harry Potter", 1, languageCode)
-            Timber.d(data.toString())
+            val movies = getSearchedMoviesUseCase("Harry Potter", 1, languageCode)
+            val series = getSearchedTvSeriesUseCase("Breaking Bad", 1, languageCode)
+            Timber.d(series.toString())
         }
     }
 }
