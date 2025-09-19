797db1ecb9f3df6881e86099fc32add8419014b9
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-19 14:30:25 +0300

    Implement search functionality in SearchFragment with RecyclerView and Paging

--- feature/search/src/main/kotlin/com/koniukhov/cinecircle/feature/search/ui/SearchFragment.kt
+++ feature/search/src/main/kotlin/com/koniukhov/cinecircle/feature/search/ui/SearchFragment.kt
@@ -1,14 +1,33 @@
 package com.koniukhov.cinecircle.feature.search.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavDeepLinkRequest
+import androidx.navigation.fragment.findNavController
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.koniukhov.cinecircle.core.common.Constants.MediaType
+import com.koniukhov.cinecircle.core.common.navigation.NavArgs.movieDetailsUri
+import com.koniukhov.cinecircle.core.common.navigation.NavArgs.tvSeriesDetailsUri
+import com.koniukhov.cinecircle.core.design.R
+import com.koniukhov.cinecircle.core.ui.adapter.PagingMediaAdapter
+import com.koniukhov.cinecircle.core.ui.utils.GridSpacingItemDecoration
 import com.koniukhov.cinecircle.feature.search.databinding.SearchFragmentBinding
 import com.koniukhov.cinecircle.feature.search.viewmodel.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SearchFragment : Fragment() {
@@ -16,6 +35,7 @@ class SearchFragment : Fragment() {
     private var _binding: SearchFragmentBinding? = null
     private val binding get() = _binding!!
     private val viewModel: SearchViewModel by viewModels ()
+    private lateinit var adapter: PagingMediaAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,14 +43,91 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = SearchFragmentBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        return root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.loadData()
+        setupRecyclerView()
+        setupSearch()
+        observePagingData()
+    }
+
+    private fun setupRecyclerView() {
+        adapter = PagingMediaAdapter { mediaId, mediaType ->
+            if (mediaType == MediaType.MOVIE) {
+                navigateToMovieDetails(mediaId)
+            } else if (mediaType == MediaType.TV_SERIES) {
+                navigateToTvSeriesDetails(mediaId)
+            }
+        }
+
+        adapter.stateRestorationPolicy =
+            RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+
+        binding.searchRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+        val spacing = resources.getDimensionPixelSize(R.dimen.grid_spacing)
+        binding.searchRecyclerView.addItemDecoration(
+            GridSpacingItemDecoration(2, spacing, true)
+        )
+        binding.searchRecyclerView.adapter = adapter
+
+        adapter.addLoadStateListener { loadStates ->
+            if (loadStates.refresh is LoadState.NotLoading) {
+                binding.searchRecyclerView.scrollToPosition(0)
+            }
+        }
+    }
+
+    private fun setupSearch() {
+        binding.searchView.editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val query = s.toString().trim()
+                viewModel.onSearchQueryChanged(query)
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        binding.searchView.editText.setOnEditorActionListener { v, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                v.clearFocus()
+                hideKeyboard(v)
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun hideKeyboard(view: View) {
+        val imm = ContextCompat.getSystemService(requireContext(), InputMethodManager::class.java)
+        imm?.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun observePagingData() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.pagingDataFlow.collectLatest { pagingData ->
+                adapter.submitData(pagingData)
+            }
+        }
+    }
+
+    private fun navigateToMovieDetails(movieId: Int) {
+        val request = NavDeepLinkRequest.Builder
+            .fromUri(movieDetailsUri(movieId))
+            .build()
+        findNavController().navigate(request)
+    }
+
+    private fun navigateToTvSeriesDetails(tvSeriesId: Int) {
+        val request = NavDeepLinkRequest.Builder
+            .fromUri(tvSeriesDetailsUri(tvSeriesId))
+            .build()
+        findNavController().navigate(request)
     }
 
     override fun onDestroyView() {
