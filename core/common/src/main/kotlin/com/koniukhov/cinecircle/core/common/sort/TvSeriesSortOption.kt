2edefe3ed182ad1a12eb0144bc02ae67aa2c03c5
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-19 17:07:57 +0300

    Add TvSeriesSortOption enum for sorting TV series by various criteria

---
+++ core/common/src/main/kotlin/com/koniukhov/cinecircle/core/common/sort/TvSeriesSortOption.kt
@@ -0,0 +1,28 @@
+package com.koniukhov.cinecircle.core.common.sort
+
+import androidx.annotation.StringRes
+import com.koniukhov.cinecircle.core.common.R
+
+enum class TvSeriesSortOption(@StringRes val labelRes: Int, val apiValue: String) {
+    FIRST_AIR_DATE_ASC(R.string.sort_first_air_date_asc, "first_air_date.asc"),
+    FIRST_AIR_DATE_DESC(R.string.sort_first_air_date_desc, "first_air_date.desc"),
+
+    NAME_ASC(R.string.sort_name_asc, "name.asc"),
+    NAME_DESC(R.string.sort_name_desc, "name.desc"),
+
+    ORIGINAL_NAME_ASC(R.string.sort_original_name_asc, "original_name.asc"),
+    ORIGINAL_NAME_DESC(R.string.sort_original_name_desc, "original_name.desc"),
+
+    POPULARITY_ASC(R.string.sort_popularity_asc, "popularity.asc"),
+    POPULARITY_DESC(R.string.sort_popularity_desc, "popularity.desc"),
+
+    VOTE_AVERAGE_ASC(R.string.sort_vote_average_asc, "vote_average.asc"),
+    VOTE_AVERAGE_DESC(R.string.sort_vote_average_desc, "vote_average.desc"),
+
+    VOTE_COUNT_ASC(R.string.sort_vote_count_asc, "vote_count.asc"),
+    VOTE_COUNT_DESC(R.string.sort_vote_count_desc, "vote_count.desc");
+
+    companion object {
+        fun fromApiValue(value: String): TvSeriesSortOption? = entries.firstOrNull { it.apiValue == value }
+    }
+}
