4eeb52ba2d599f4f7fa43ed0856ffd9fdab0a7df
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-19 13:58:45 +0300

    Refactor SearchViewModel to implement search query handling with PagingData

--- feature/search/src/main/kotlin/com/koniukhov/cinecircle/feature/search/viewmodel/SearchViewModel.kt
+++ feature/search/src/main/kotlin/com/koniukhov/cinecircle/feature/search/viewmodel/SearchViewModel.kt
@@ -1,28 +1,40 @@
 package com.koniukhov.cinecircle.feature.search.viewmodel
 
-import android.R.attr.data
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.koniukhov.cinecircle.core.data.di.LanguageCode
-import com.koniukhov.cinecircle.core.domain.usecase.GetSearchedMoviesUseCase
-import com.koniukhov.cinecircle.core.domain.usecase.GetSearchedTvSeriesUseCase
+import com.koniukhov.cinecircle.feature.search.repository.SearchRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
-    private val getSearchedMoviesUseCase: GetSearchedMoviesUseCase,
-    private val getSearchedTvSeriesUseCase: GetSearchedTvSeriesUseCase,
+    private val repository: SearchRepository,
     @LanguageCode
     private val languageCode: String
 ) : ViewModel() {
-    fun loadData(){
+    private val searchQuery = MutableStateFlow("")
+    val pagingDataFlow = searchQuery
+        .debounce(300)
+        .distinctUntilChanged()
+        .flatMapLatest { q ->
+            val query = q.trim()
+            if (query.isBlank()) flowOf(PagingData.empty())
+            else repository.getSearchFlow(query, languageCode)
+        }
+        .cachedIn(viewModelScope)
+
+    fun onSearchQueryChanged(query: String) {
         viewModelScope.launch {
-            val movies = getSearchedMoviesUseCase("Harry Potter", 1, languageCode)
-            val series = getSearchedTvSeriesUseCase("Breaking Bad", 1, languageCode)
-            Timber.d(series.toString())
+            searchQuery.emit(query)
         }
     }
 }
