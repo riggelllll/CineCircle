f7350b89671b3ab790722d80df99fd43e5ec3674
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-25 10:37:44 +0300

    Add TV series filter state and paging flow to SearchViewModel

--- feature/search/src/main/kotlin/com/koniukhov/cinecircle/feature/search/viewmodel/SearchViewModel.kt
+++ feature/search/src/main/kotlin/com/koniukhov/cinecircle/feature/search/viewmodel/SearchViewModel.kt
@@ -10,6 +10,7 @@ import com.koniukhov.cinecircle.core.data.di.LanguageCode
 import com.koniukhov.cinecircle.core.domain.usecase.GetMovieGenresUseCase
 import com.koniukhov.cinecircle.core.domain.usecase.GetTvSeriesGenresUseCase
 import com.koniukhov.cinecircle.feature.search.model.MovieFilterParams
+import com.koniukhov.cinecircle.feature.search.model.TvSeriesFilterParams
 import com.koniukhov.cinecircle.feature.search.repository.FilteredMediaRepository
 import com.koniukhov.cinecircle.feature.search.repository.SearchRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -54,6 +55,9 @@ class SearchViewModel @Inject constructor(
     private var _moviesFilterParamsState = MutableStateFlow<MovieFilterParams?>(null)
     val moviesFilterParamsState = _moviesFilterParamsState.asStateFlow()
 
+    private var _tvSeriesFilterParamsState = MutableStateFlow<TvSeriesFilterParams?>(null)
+    val tvSeriesFilterParamsState = _tvSeriesFilterParamsState.asStateFlow()
+
     val moviesFilterPagingDataFlow = _moviesFilterParamsState
         .flatMapLatest { params ->
             if (params == null) {
@@ -78,6 +82,31 @@ class SearchViewModel @Inject constructor(
         }
         .cachedIn(viewModelScope)
 
+    val tvSeriesFilterPagingDataFlow = _tvSeriesFilterParamsState
+        .flatMapLatest { params ->
+            if (params == null) {
+                flowOf(PagingData.empty())
+            } else {
+                filteredMediaRepository.getFilteredTvSeriesFlow(
+                    language = languageCode,
+                    sortBy = params.sortBy,
+                    year = params.year,
+                    firstAirDateGte = params.firstAirDateGte,
+                    firstAirDateLte = params.firstAirDateLte,
+                    airDateGte = params.airDateGte,
+                    airDateLte = params.airDateLte,
+                    minVoteAverage = params.minVoteAverage,
+                    maxVoteAverage = params.maxVoteAverage,
+                    minVoteCount = params.minVoteCount,
+                    maxVoteCount = params.maxVoteCount,
+                    withOriginCountry = params.withOriginCountry,
+                    withOriginalLanguage = params.withOriginalLanguage,
+                    withGenres = params.withGenres,
+                    withoutGenres = params.withoutGenres
+                )
+            }
+        }.cachedIn(viewModelScope)
+
     init {
         setUpGenres()
         setupLanguagesAndCountries()
@@ -101,7 +130,29 @@ class SearchViewModel @Inject constructor(
         countries = Locale.getDefault().getLocalizedCountryMap()
     }
 
-    fun updateMovieFilters(params: MovieFilterParams?) {
+     fun selectMovieFilters(params: MovieFilterParams?) {
+         resetTvSeriesFilters()
+         updateMovieFilters(params)
+    }
+
+    private fun resetTvSeriesFilters() {
+        viewModelScope.launch { _tvSeriesFilterParamsState.emit(null) }
+    }
+
+    private fun updateMovieFilters(params: MovieFilterParams?) {
         viewModelScope.launch { _moviesFilterParamsState.emit(params) }
+    }
+
+    fun selectTvSeriesFilters(params: TvSeriesFilterParams?) {
+        resetMovieFilters()
+        updateTvSeriesFilters(params)
+    }
+
+    private fun resetMovieFilters() {
+        viewModelScope.launch { _moviesFilterParamsState.emit(null) }
+    }
+
+    private fun updateTvSeriesFilters(params: TvSeriesFilterParams?) {
+        viewModelScope.launch { _tvSeriesFilterParamsState.emit(params) }
     }
 }
