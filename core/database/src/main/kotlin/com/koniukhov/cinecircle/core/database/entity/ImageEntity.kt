936ab7f53e482d4c3184544fa155e198d067b4d2
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-25 16:50:14 +0300

    Add ImageEntity data class for storing media image information in database

---
+++ core/database/src/main/kotlin/com/koniukhov/cinecircle/core/database/entity/ImageEntity.kt
@@ -0,0 +1,13 @@
+package com.koniukhov.cinecircle.core.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ImageEntity (
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    val mediaId: Long,
+    var filePath: String,
+    var imageType: ImageType
+)
