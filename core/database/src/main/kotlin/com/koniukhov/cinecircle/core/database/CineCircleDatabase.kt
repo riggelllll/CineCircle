05b1f7eca5be918089b75b1adf495f5a6909cb8c
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-25 16:52:24 +0300

    Add ImageTypeConverter to CineCircleDatabase for type conversion support

--- core/database/src/main/kotlin/com/koniukhov/cinecircle/core/database/CineCircleDatabase.kt
+++ core/database/src/main/kotlin/com/koniukhov/cinecircle/core/database/CineCircleDatabase.kt
@@ -2,6 +2,8 @@ package com.koniukhov.cinecircle.core.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.koniukhov.cinecircle.core.database.converter.ImageTypeConverter
 import com.koniukhov.cinecircle.core.database.dao.MediaListDao
 import com.koniukhov.cinecircle.core.database.entity.MediaListEntity
 import com.koniukhov.cinecircle.core.database.entity.MediaListItemEntity
@@ -14,6 +16,7 @@ import com.koniukhov.cinecircle.core.database.entity.MediaListItemEntity
     version = 1,
     exportSchema = false
 )
+@TypeConverters(ImageTypeConverter::class)
 abstract class CineCircleDatabase : RoomDatabase() {
 
     abstract fun mediaListDao(): MediaListDao
