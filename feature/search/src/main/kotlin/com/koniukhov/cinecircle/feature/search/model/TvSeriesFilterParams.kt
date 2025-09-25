ac7830f5868cefde1c97a8ed2fb85a0e06311458
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-25 09:57:22 +0300

    Add TvSeriesFilterParams data model for TV series filtering criteria

---
+++ feature/search/src/main/kotlin/com/koniukhov/cinecircle/feature/search/model/TvSeriesFilterParams.kt
@@ -0,0 +1,18 @@
+package com.koniukhov.cinecircle.feature.search.model
+
+data class TvSeriesFilterParams (
+    val sortBy: String? = null,
+    val airDateGte: String?,
+    val airDateLte: String? = null,
+    val year: Int? = null,
+    val firstAirDateGte: String? = null,
+    val firstAirDateLte: String? = null,
+    val minVoteAverage: Float? = null,
+    val maxVoteAverage: Float? = null,
+    val minVoteCount: Int? = null,
+    val maxVoteCount: Int? = null,
+    val withOriginCountry: String? = null,
+    val withOriginalLanguage: String? = null,
+    val withGenres: String? = null,
+    val withoutGenres: String? = null
+)
