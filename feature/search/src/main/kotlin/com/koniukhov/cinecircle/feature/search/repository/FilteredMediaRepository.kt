28e1b843c863ea4b08d70bc609d0d5e4fa7ddc16
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-25 09:07:55 +0300

    Add getFilteredTvSeriesFlow method to FilteredMediaRepository for paginated TV series filtering

--- feature/search/src/main/kotlin/com/koniukhov/cinecircle/feature/search/repository/FilteredMediaRepository.kt
+++ feature/search/src/main/kotlin/com/koniukhov/cinecircle/feature/search/repository/FilteredMediaRepository.kt
@@ -55,6 +55,50 @@ class FilteredMediaRepository @Inject constructor(
         ).flow
     }
 
+    fun getFilteredTvSeriesFlow(
+        language: String,
+        sortBy: String?,
+        airDateGte: String?,
+        airDateLte: String?,
+        year: Int?,
+        firstAirDateGte: String?,
+        firstAirDateLte: String?,
+        minVoteAverage: Float?,
+        maxVoteAverage: Float?,
+        minVoteCount: Int?,
+        maxVoteCount: Int?,
+        withOriginCountry: String?,
+        withOriginalLanguage: String?,
+        withGenres: String?,
+        withoutGenres: String?
+    ) : Flow<PagingData<MediaItem>> {
+        val tvSeriesUseCase = filteredTvSeriesUseCase::invoke
+
+        return Pager(
+            config = PagingConfig(pageSize = PAGE_SIZE),
+            pagingSourceFactory = {
+                com.koniukhov.cinecircle.feature.search.paging.FilteredTvSeriesSource(
+                    filteredTvSeriesUseCase = tvSeriesUseCase,
+                    language = language,
+                    sortBy = sortBy ?: MovieSortOption.POPULARITY_DESC.apiValue,
+                    airDateGte = airDateGte,
+                    airDateLte = airDateLte,
+                    year = year,
+                    firstAirDateGte = firstAirDateGte,
+                    firstAirDateLte = firstAirDateLte,
+                    minVoteAverage = minVoteAverage,
+                    maxVoteAverage = maxVoteAverage,
+                    minVoteCount = minVoteCount,
+                    maxVoteCount = maxVoteCount,
+                    withOriginCountry = withOriginCountry,
+                    withOriginalLanguage = withOriginalLanguage,
+                    withGenres = withGenres,
+                    withoutGenres = withoutGenres
+                )
+            }
+        ).flow
+    }
+
     companion object{
         const val PAGE_SIZE = 20
     }
