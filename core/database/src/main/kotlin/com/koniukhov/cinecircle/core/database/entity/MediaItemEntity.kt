a780a83d2ea6fe8ef04ec9685b1d29ce5a1ff7f8
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-26 15:50:00 +0300

    Rename ImageEntity to MediaItemEntity to better reflect entity purpose

--- core/database/src/main/kotlin/com/koniukhov/cinecircle/core/database/entity/MediaItemEntity.kt
+++ core/database/src/main/kotlin/com/koniukhov/cinecircle/core/database/entity/MediaItemEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class ImageEntity (
+data class MediaItemEntity (
     @PrimaryKey(autoGenerate = true)
     val id: Long,
     val mediaId: Long,
