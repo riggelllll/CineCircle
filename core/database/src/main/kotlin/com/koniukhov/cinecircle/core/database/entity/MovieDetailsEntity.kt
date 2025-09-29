0504a97cff3898467f214e890959d0bdd18df3c7
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-29 16:09:19 +0300

    Add MovieDetailsEntity for Room database with table name "movie_details"

---
+++ core/database/src/main/kotlin/com/koniukhov/cinecircle/core/database/entity/MovieDetailsEntity.kt
@@ -0,0 +1,30 @@
+package com.koniukhov.cinecircle.core.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "movie_details")
+data class MovieDetailsEntity(
+    @PrimaryKey(autoGenerate = true)
+    val uid: Int = 0,
+    val mediaId: Int,
+    val adult: Boolean,
+    val backdropPath: String,
+    val budget: Int,
+    val homePage: String,
+    val id: Int,
+    val imdbId: String,
+    val originalLanguage: String,
+    val originalTitle: String,
+    val overview: String,
+    val popularity: Float,
+    val posterPath: String,
+    val releaseDate: String,
+    val revenue: Long,
+    val runtime: Int,
+    val status: String,
+    val tagline: String,
+    val title: String,
+    val voteAverage: Float,
+    val voteCount: Int
+)
