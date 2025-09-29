79fa6c97a2b89270dc4046cb16b1a21d9eb503e5
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-09-29 15:41:41 +0300

    Specify table name as "images" in ImageEntity annotation

--- core/database/src/main/kotlin/com/koniukhov/cinecircle/core/database/entity/ImageEntity.kt
+++ core/database/src/main/kotlin/com/koniukhov/cinecircle/core/database/entity/ImageEntity.kt
@@ -3,7 +3,7 @@ package com.koniukhov.cinecircle.core.database.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "images")
 data class ImageEntity (
     @PrimaryKey(autoGenerate = true)
     val id: Long,
