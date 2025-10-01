9328084ad28b50af5bcc3d0a2e0ee8f990434c0f
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-01 17:32:21 +0300

    Refactor ReleaseDatesRepositoryImpl to use fetchWithLocalAndRetry for movie release date retrieval

--- core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/repository/ReleaseDatesRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/repository/ReleaseDatesRepositoryImpl.kt
@@ -2,13 +2,23 @@ package com.koniukhov.cinecircle.core.data.repository
 
 import com.koniukhov.cinecircle.core.data.mapper.toDomain
 import com.koniukhov.cinecircle.core.data.remote.RemoteRemoteReleaseDatesDataSourceImpl
+import com.koniukhov.cinecircle.core.data.util.fetchWithLocalAndRetry
+import com.koniukhov.cinecircle.core.domain.NetworkStatusProvider
 import com.koniukhov.cinecircle.core.domain.model.ReleaseDateResult
 import com.koniukhov.cinecircle.core.domain.repository.ReleaseDatesRepository
 import javax.inject.Inject
 
-class ReleaseDatesRepositoryImpl @Inject constructor(private val remoteReleaseDatesDataSourceImpl: RemoteRemoteReleaseDatesDataSourceImpl) : ReleaseDatesRepository {
+class ReleaseDatesRepositoryImpl @Inject constructor(
+    private val remoteReleaseDatesDataSourceImpl: RemoteRemoteReleaseDatesDataSourceImpl,
+    private val networkStatusProvider: NetworkStatusProvider
+) : ReleaseDatesRepository {
     override suspend fun getMovieReleaseDates(movieId: Int): List<ReleaseDateResult> {
-        val dto = remoteReleaseDatesDataSourceImpl.getMovieReleaseDates(movieId)
-        return dto.results.map { it.toDomain() }
+        return fetchWithLocalAndRetry(
+            remoteCall = {
+                val dto = remoteReleaseDatesDataSourceImpl.getMovieReleaseDates(movieId)
+                dto.results.map { it.toDomain() } },
+            localCall = { emptyList() },
+            isNetworkAvailable = { networkStatusProvider.isNetworkAvailable() }
+        ) ?: emptyList()
     }
 }
