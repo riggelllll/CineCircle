3f9e07a2e545434b168cbe84b3c43f68061bbb1f
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-01 17:25:55 +0300

    Refactor GenresRepositoryImpl to use fetchWithLocalAndRetry for genre list retrieval

--- core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/repository/GenresRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/repository/GenresRepositoryImpl.kt
@@ -2,18 +2,33 @@ package com.koniukhov.cinecircle.core.data.repository
 
 import com.koniukhov.cinecircle.core.data.mapper.toDomain
 import com.koniukhov.cinecircle.core.data.remote.RemoteRemoteGenresDataSourceImpl
+import com.koniukhov.cinecircle.core.data.util.fetchWithLocalAndRetry
+import com.koniukhov.cinecircle.core.domain.NetworkStatusProvider
 import com.koniukhov.cinecircle.core.domain.model.Genre
 import com.koniukhov.cinecircle.core.domain.repository.GenresRepository
 import javax.inject.Inject
 
-class GenresRepositoryImpl @Inject constructor(private val remoteGenresDataSourceImpl: RemoteRemoteGenresDataSourceImpl) : GenresRepository{
+class GenresRepositoryImpl @Inject constructor(
+    private val remoteGenresDataSourceImpl: RemoteRemoteGenresDataSourceImpl,
+    private val networkStatusProvider: NetworkStatusProvider
+) : GenresRepository{
     override suspend fun getMoviesGenreList(language: String): List<Genre> {
-        val dto = remoteGenresDataSourceImpl.getMoviesGenreList(language)
-        return dto.map { it.toDomain() }
+        return fetchWithLocalAndRetry(
+            remoteCall = {
+                val dto = remoteGenresDataSourceImpl.getMoviesGenreList(language)
+                dto.map { it.toDomain() } },
+            localCall = { null },
+            isNetworkAvailable = { networkStatusProvider.isNetworkAvailable() }
+        ) ?: emptyList()
     }
 
     override suspend fun getTvSeriesGenreList(language: String): List<Genre> {
-        val dto = remoteGenresDataSourceImpl.getTvSeriesGenreList(language)
-        return dto.map { it.toDomain() }
+        return fetchWithLocalAndRetry(
+            remoteCall = {
+                val dto = remoteGenresDataSourceImpl.getTvSeriesGenreList(language)
+                dto.map { it.toDomain() } },
+            localCall = { null },
+            isNetworkAvailable = { networkStatusProvider.isNetworkAvailable() }
+        ) ?: emptyList()
     }
 }
