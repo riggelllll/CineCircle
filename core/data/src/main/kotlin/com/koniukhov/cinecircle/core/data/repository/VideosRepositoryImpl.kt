937ba6be9d7c9d1ea4b9b2cf469cb401ad898231
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-01 17:27:22 +0300

    Refactor VideosRepositoryImpl to use fetchWithLocalAndRetry for video retrieval

--- core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/repository/VideosRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/repository/VideosRepositoryImpl.kt
@@ -2,24 +2,39 @@ package com.koniukhov.cinecircle.core.data.repository
 
 import com.koniukhov.cinecircle.core.data.mapper.toDomain
 import com.koniukhov.cinecircle.core.data.remote.RemoteRemoteVideosDataSourceImpl
+import com.koniukhov.cinecircle.core.data.util.fetchWithLocalAndRetry
+import com.koniukhov.cinecircle.core.domain.NetworkStatusProvider
 import com.koniukhov.cinecircle.core.domain.model.MovieVideos
 import com.koniukhov.cinecircle.core.domain.repository.VideosRepository
 import javax.inject.Inject
 
-class VideosRepositoryImpl @Inject constructor(private val remoteVideosDataSourceImpl: RemoteRemoteVideosDataSourceImpl) : VideosRepository{
+class VideosRepositoryImpl @Inject constructor(
+    private val remoteVideosDataSourceImpl: RemoteRemoteVideosDataSourceImpl,
+    private val networkStatusProvider: NetworkStatusProvider
+) : VideosRepository{
     override suspend fun getMovieVideos(
         movieId: Int,
         language: String
     ): MovieVideos {
-        val dto = remoteVideosDataSourceImpl.getMovieVideos(movieId, language)
-        return dto.toDomain()
+        return fetchWithLocalAndRetry(
+            remoteCall = {
+                val dto = remoteVideosDataSourceImpl.getMovieVideos(movieId, language)
+                dto.toDomain() },
+            localCall = { MovieVideos.empty() },
+            isNetworkAvailable = { networkStatusProvider.isNetworkAvailable() }
+        ) ?: MovieVideos.empty()
     }
 
     override suspend fun getTvSeriesVideos(
         tvSeriesId: Int,
         language: String
     ): MovieVideos {
-        val dto = remoteVideosDataSourceImpl.getTvSeriesVideos(tvSeriesId, language)
-        return dto.toDomain()
+        return fetchWithLocalAndRetry(
+            remoteCall = {
+                val dto = remoteVideosDataSourceImpl.getTvSeriesVideos(tvSeriesId, language)
+                dto.toDomain() },
+            localCall = { MovieVideos.empty() },
+            isNetworkAvailable = { networkStatusProvider.isNetworkAvailable() }
+        ) ?: MovieVideos.empty()
     }
 }
