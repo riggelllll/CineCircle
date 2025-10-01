466dcf73a97e4b35bac38a126822faad4e506bdc
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-01 14:54:56 +0300

    Add companion object to MediaCredits for creating an empty instance

--- core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/model/MediaCredits.kt
+++ core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/model/MediaCredits.kt
@@ -3,4 +3,8 @@ package com.koniukhov.cinecircle.core.domain.model
 data class MediaCredits(
     val cast: List<CastMember>,
     val crew: List<CrewMember>
-)
+) {
+    companion object {
+        fun empty() = MediaCredits(emptyList(), emptyList())
+    }
+}
