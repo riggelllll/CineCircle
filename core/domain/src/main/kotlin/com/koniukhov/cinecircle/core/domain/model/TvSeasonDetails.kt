ca1f450a67541ed70308c38b58a4c355e4b6e4b8
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-01 14:59:21 +0300

    Add companion object to TvSeasonDetails for creating an empty instance

--- core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/model/TvSeasonDetails.kt
+++ core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/model/TvSeasonDetails.kt
@@ -1,5 +1,7 @@
 package com.koniukhov.cinecircle.core.domain.model
 
+import com.koniukhov.cinecircle.core.common.Constants.INVALID_ID
+
 data class TvSeasonDetails(
     val _id: String,
     val airDate: String,
@@ -10,4 +12,18 @@ data class TvSeasonDetails(
     val posterPath: String,
     val seasonNumber: Int,
     val voteAverage: Float
-)
+) {
+    companion object {
+        fun empty() = TvSeasonDetails(
+            _id = "",
+            airDate = "",
+            episodes = emptyList(),
+            name = "",
+            overview = "",
+            id = INVALID_ID,
+            posterPath = "",
+            seasonNumber = 0,
+            voteAverage = 0f
+        )
+    }
+}
