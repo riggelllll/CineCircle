403d46bbf5605b475bfeb79332ebad68aeddf750
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-01 14:56:48 +0300

    Add companion object to MediaImages for creating an empty instance

--- core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/model/MediaImages.kt
+++ core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/model/MediaImages.kt
@@ -1,8 +1,19 @@
 package com.koniukhov.cinecircle.core.domain.model
 
+import com.koniukhov.cinecircle.core.common.Constants.INVALID_ID
+
 data class MediaImages(
     val id: Int,
     val backdrops: List<Image>,
     val logos: List<Image>,
     val posters: List<Image>
-)
+) {
+    companion object {
+        fun empty() = MediaImages(
+            id = INVALID_ID,
+            backdrops = emptyList(),
+            logos = emptyList(),
+            posters = emptyList()
+        )
+    }
+}
