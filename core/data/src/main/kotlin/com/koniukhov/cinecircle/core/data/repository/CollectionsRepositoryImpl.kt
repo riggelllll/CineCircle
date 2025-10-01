e0321042b64d74820a890f2a60c4803131764e41
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-01 15:01:27 +0300

    Refactor CollectionsRepositoryImpl to use fetchWithLocalAndRetry for collection details retrieval

--- core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/repository/CollectionsRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/repository/CollectionsRepositoryImpl.kt
@@ -2,16 +2,26 @@ package com.koniukhov.cinecircle.core.data.repository
 
 import com.koniukhov.cinecircle.core.data.mapper.toDomain
 import com.koniukhov.cinecircle.core.data.remote.RemoteRemoteCollectionsDataSourceImpl
+import com.koniukhov.cinecircle.core.data.util.fetchWithLocalAndRetry
+import com.koniukhov.cinecircle.core.domain.NetworkStatusProvider
 import com.koniukhov.cinecircle.core.domain.model.CollectionDetails
 import com.koniukhov.cinecircle.core.domain.repository.CollectionsRepository
 import javax.inject.Inject
 
-class CollectionsRepositoryImpl @Inject constructor(private val remoteDataSource: RemoteRemoteCollectionsDataSourceImpl) : CollectionsRepository {
+class CollectionsRepositoryImpl @Inject constructor(
+    private val remoteDataSource: RemoteRemoteCollectionsDataSourceImpl,
+    private val networkStatusProvider: NetworkStatusProvider
+) : CollectionsRepository {
     override suspend fun getCollectionDetails(
         collectionId: Int,
         language: String
     ): CollectionDetails {
-        val tdo = remoteDataSource.getCollectionDetails(collectionId, language)
-        return tdo.toDomain()
+        return fetchWithLocalAndRetry(
+            remoteCall = {
+                val tdo = remoteDataSource.getCollectionDetails(collectionId, language)
+                tdo.toDomain() },
+            localCall = { CollectionDetails.empty() },
+            isNetworkAvailable = { networkStatusProvider.isNetworkAvailable() }
+        ) ?: CollectionDetails.empty()
     }
 }
