e060ef68be58661ead3249e826bfaece26b2f4d8
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-02 16:04:26 +0300

    Update RemoteVideosDataSource to use MediaVideos for consistency in naming

--- core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/remote/RemoteVideosDataSource.kt
+++ core/data/src/main/kotlin/com/koniukhov/cinecircle/core/data/remote/RemoteVideosDataSource.kt
@@ -1,8 +1,8 @@
 package com.koniukhov.cinecircle.core.data.remote
 
-import com.koniukhov.cinecircle.core.network.model.MovieVideosDto
+import com.koniukhov.cinecircle.core.network.model.MediaVideosDto
 
 interface RemoteVideosDataSource {
-    suspend fun getMovieVideos(movieId: Int, language: String): MovieVideosDto
-    suspend fun getTvSeriesVideos(tvSeriesId: Int, language: String): MovieVideosDto
+    suspend fun getMovieVideos(movieId: Int, language: String): MediaVideosDto
+    suspend fun getTvSeriesVideos(tvSeriesId: Int, language: String): MediaVideosDto
 }
