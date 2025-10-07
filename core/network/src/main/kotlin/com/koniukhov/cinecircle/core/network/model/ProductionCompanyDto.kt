29d7757257bfa465ad60a5ce47925acb62a5c6db
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-07 18:00:08 +0300

    Make properties nullable in ProductionCompanyDto

--- core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/ProductionCompanyDto.kt
+++ core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/ProductionCompanyDto.kt
@@ -3,10 +3,10 @@ package com.koniukhov.cinecircle.core.network.model
 import com.google.gson.annotations.SerializedName
 
 data class ProductionCompanyDto(
-    val id: Int,
+    val id: Int?,
     @SerializedName("logo_path")
-    val logoPath: String,
-    val name: String,
+    val logoPath: String?,
+    val name: String?,
     @SerializedName("origin_country")
-    val originCountry: String
+    val originCountry: String?
 )
