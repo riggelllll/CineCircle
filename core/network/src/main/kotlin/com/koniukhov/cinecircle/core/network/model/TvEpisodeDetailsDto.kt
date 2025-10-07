7d59219456a449280140a46425622b0d59bc8dd8
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-07 19:19:04 +0300

    Make properties nullable in TvEpisodeDetailsDto

--- core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/TvEpisodeDetailsDto.kt
+++ core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/TvEpisodeDetailsDto.kt
@@ -5,26 +5,26 @@ import com.koniukhov.cinecircle.core.common.Constants.INVALID_ID
 
 data class TvEpisodeDetailsDto(
     @SerializedName("air_date")
-    val airDate: String,
-    val crew: List<CrewMemberDto>,
+    val airDate: String?,
+    val crew: List<CrewMemberDto>?,
     @SerializedName("episode_number")
-    val episodeNumber: Int,
+    val episodeNumber: Int?,
     @SerializedName("guest_stars")
-    val guestStars: List<CastMemberDto>,
-    val name: String,
-    val overview: String,
-    val id: Int,
+    val guestStars: List<CastMemberDto>?,
+    val name: String?,
+    val overview: String?,
+    val id: Int?,
     @SerializedName("production_code")
-    val productionCode: String,
-    val runtime: Int,
+    val productionCode: String?,
+    val runtime: Int?,
     @SerializedName("season_number")
-    val seasonNumber: Int,
+    val seasonNumber: Int?,
     @SerializedName("still_path")
-    val stillPath: String,
+    val stillPath: String?,
     @SerializedName("vote_average")
-    val voteAverage: Float,
+    val voteAverage: Float?,
     @SerializedName("vote_count")
-    val voteCount: Int
+    val voteCount: Int?
 ){
     companion object{
         fun empty() = TvEpisodeDetailsDto(
