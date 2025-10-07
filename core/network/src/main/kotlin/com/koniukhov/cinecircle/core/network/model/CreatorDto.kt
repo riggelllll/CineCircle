d01cec62b389aed9c343e214d60c347880b06a19
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-07 15:43:33 +0300

    Make id, creditId, name, gender, and profilePath properties nullable in CreatorDto

--- core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/CreatorDto.kt
+++ core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/CreatorDto.kt
@@ -4,13 +4,13 @@ import com.google.gson.annotations.SerializedName
 import com.koniukhov.cinecircle.core.common.Constants.INVALID_ID
 
 data class CreatorDto(
-    val id: Int = 0,
+    val id: Int?,
     @SerializedName("credit_id")
-    val creditId: String,
-    val name: String,
-    val gender: Int = 0,
+    val creditId: String?,
+    val name: String?,
+    val gender: Int?,
     @SerializedName("profile_path")
-    val profilePath: String
+    val profilePath: String?
 ){
     companion object{
         fun empty() = CreatorDto(INVALID_ID, "", "", 0, "")
