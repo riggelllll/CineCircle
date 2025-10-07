a50616b28bef6891e1a49099c34657d074b87376
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-07 15:54:44 +0300

    Rename isoName to languageCode in Language data class

--- core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/model/Language.kt
+++ core/domain/src/main/kotlin/com/koniukhov/cinecircle/core/domain/model/Language.kt
@@ -1,7 +1,7 @@
 package com.koniukhov.cinecircle.core.domain.model
 
 data class Language(
-    val isoName: String,
+    val languageCode: String,
     val englishName: String,
     val name: String
 )
