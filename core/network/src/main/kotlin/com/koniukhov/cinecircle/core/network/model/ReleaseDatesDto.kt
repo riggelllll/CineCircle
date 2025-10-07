9ab524fe500793b1ed484595fd8c0f75d7664592
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-07 19:12:49 +0300

    Make properties nullable in ReleaseDatesDto

--- core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/ReleaseDatesDto.kt
+++ core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/ReleaseDatesDto.kt
@@ -3,12 +3,12 @@ package com.koniukhov.cinecircle.core.network.model
 import com.google.gson.annotations.SerializedName
 
 data class ReleaseDatesDto(
-    val certification: String,
-    val descriptors: List<String>,
+    val certification: String?,
+    val descriptors: List<String>?,
     @SerializedName("iso_639_1")
-    val languageCode: String,
-    val note: String,
+    val languageCode: String?,
+    val note: String?,
     @SerializedName("release_date")
-    val releaseDate: String,
-    val type: Int = 0
+    val releaseDate: String?,
+    val type: Int?
 )
