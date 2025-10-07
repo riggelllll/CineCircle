af7d1df57965a11b4b8d0fa4187f6276a5198388
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-07 19:30:22 +0300

    Make properties nullable in VideoDto

--- core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/VideoDto.kt
+++ core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/VideoDto.kt
@@ -4,16 +4,16 @@ import com.google.gson.annotations.SerializedName
 
 data class VideoDto(
     @SerializedName("iso_639_1")
-    val languageCode: String,
+    val languageCode: String?,
     @SerializedName("iso_3166_1")
-    val countryCode: String,
-    val name: String,
-    val key: String,
-    val site: String,
-    val size: Int,
-    val type: String,
-    val official: Boolean,
+    val countryCode: String?,
+    val name: String?,
+    val key: String?,
+    val site: String?,
+    val size: Int?,
+    val type: String?,
+    val official: Boolean?,
     @SerializedName("published_at")
-    val publishedAt: String,
-    val id: String
+    val publishedAt: String?,
+    val id: String?
 )
