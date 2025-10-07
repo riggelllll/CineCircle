b565eb98b88ff79c1b3474b7af7bb0aca2224578
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-07 16:04:11 +0300

    Make id, backdrops, logos, and posters properties nullable in MediaImagesDto

--- core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/MediaImagesDto.kt
+++ core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/MediaImagesDto.kt
@@ -3,10 +3,10 @@ package com.koniukhov.cinecircle.core.network.model
 import com.koniukhov.cinecircle.core.common.Constants.INVALID_ID
 
 data class MediaImagesDto(
-    val id: Int,
-    val backdrops: List<ImageDto>,
-    val logos: List<ImageDto>,
-    val posters: List<ImageDto>
+    val id: Int?,
+    val backdrops: List<ImageDto>?,
+    val logos: List<ImageDto>?,
+    val posters: List<ImageDto>?
 ){
     companion object{
         fun empty() = MediaImagesDto(INVALID_ID, emptyList(), emptyList(), emptyList())
