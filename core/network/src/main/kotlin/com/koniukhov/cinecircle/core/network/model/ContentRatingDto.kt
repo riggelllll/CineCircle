bd4441448b06b2bf951f42506f8cc805eb8b5fed
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-07 15:38:42 +0300

    Make descriptors, countryCode, and rating properties nullable in ContentRatingDto

--- core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/ContentRatingDto.kt
+++ core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/ContentRatingDto.kt
@@ -3,8 +3,8 @@ package com.koniukhov.cinecircle.core.network.model
 import com.google.gson.annotations.SerializedName
 
 data class ContentRatingDto (
-    val descriptors: List<String>,
+    val descriptors: List<String>?,
     @SerializedName("iso_3166_1")
-    val countryCode: String,
-    val rating: String
+    val countryCode: String?,
+    val rating: String?
 )
