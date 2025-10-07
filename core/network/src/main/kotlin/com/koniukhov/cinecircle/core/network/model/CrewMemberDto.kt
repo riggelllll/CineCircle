286363a643cc2ad39877b0dd79866b8d04ab5308
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-07 15:45:53 +0300

    Make properties nullable in CrewMemberDto and change popularity type from Double to Float

--- core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/CrewMemberDto.kt
+++ core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/CrewMemberDto.kt
@@ -3,19 +3,19 @@ package com.koniukhov.cinecircle.core.network.model
 import com.google.gson.annotations.SerializedName
 
 data class CrewMemberDto (
-    val adult: Boolean = true,
-    val gender: Int = 0,
-    val id: Int = 0,
+    val adult: Boolean?,
+    val gender: Int?,
+    val id: Int?,
     @SerializedName("known_for_department")
-    val knownForDepartment: String,
-    val name: String,
+    val knownForDepartment: String?,
+    val name: String?,
     @SerializedName("original_name")
-    val originalName: String,
-    val popularity: Double = 0.0,
+    val originalName: String?,
+    val popularity: Float?,
     @SerializedName("profile_path")
-    val profilePath: String,
+    val profilePath: String?,
     @SerializedName("credit_id")
-    val creditId: String,
-    val department: String,
-    val job: String
+    val creditId: String?,
+    val department: String?,
+    val job: String?
 )
