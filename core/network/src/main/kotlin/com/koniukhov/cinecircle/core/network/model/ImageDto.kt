efd705f5bea3836c0a4e822c9f84e36e0ebd59c7
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-10-07 15:51:50 +0300

    Change aspectRatio and voteAverage types to Float and make properties nullable in ImageDto

--- core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/ImageDto.kt
+++ core/network/src/main/kotlin/com/koniukhov/cinecircle/core/network/model/ImageDto.kt
@@ -4,15 +4,15 @@ import com.google.gson.annotations.SerializedName
 
 data class ImageDto(
     @SerializedName("aspect_ratio")
-    val aspectRatio: Double,
+    val aspectRatio: Float?,
     @SerializedName("file_path")
-    val filePath: String,
-    val height: Int,
+    val filePath: String?,
+    val height: Int?,
     @SerializedName("iso_639_1")
-    val countryCode: String,
+    val countryCode: String?,
     @SerializedName("vote_average")
-    val voteAverage: Double,
+    val voteAverage: Float?,
     @SerializedName("vote_count")
-    val voteCount: Int,
-    val width: Int
+    val voteCount: Int?,
+    val width: Int?
 )
