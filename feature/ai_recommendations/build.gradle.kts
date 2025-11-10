c54119516e5c50055a8482fae2974cf12cd4ad0d
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-11-10 20:01:06 +0200

    Add database dependency to build.gradle.kts

--- feature/ai_recommendations/build.gradle.kts
+++ feature/ai_recommendations/build.gradle.kts
@@ -10,6 +10,7 @@ android {
 dependencies {
     implementation(projects.core.common)
     implementation(projects.core.ui)
+    implementation(projects.core.database)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.recyclerview)
