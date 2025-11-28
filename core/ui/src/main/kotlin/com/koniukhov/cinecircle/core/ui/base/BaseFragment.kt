fcee0f8f2015275d7326df82843cd7b6f139c711
Vladyslav Koniukhov <riggelllll@gmail.com>
2025-11-28 19:31:12 +0200

    Add BaseFragment class for consistent fragment implementation with ViewBinding and ViewModel

---
+++ core/ui/src/main/kotlin/com/koniukhov/cinecircle/core/ui/base/BaseFragment.kt
@@ -0,0 +1,68 @@
+package com.koniukhov.cinecircle.core.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import androidx.viewbinding.ViewBinding
+import kotlinx.coroutines.launch
+
+abstract class BaseFragment<VB : ViewBinding, VM : ViewModel> : Fragment() {
+
+    private var _binding: VB? = null
+    protected val binding get() = _binding!!
+
+    protected abstract val viewModel: VM
+
+    protected abstract fun createBinding(inflater: LayoutInflater, container: ViewGroup?): VB
+
+    protected abstract fun initViews()
+
+    protected abstract fun observeViewModel()
+
+    protected open fun setupViews() {}
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = createBinding(inflater, container)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        setupViews()
+        observeViewModel()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    protected fun safeUiCall(action: () -> Unit) {
+        if (isAdded && view != null) {
+            action()
+        }
+    }
+
+    protected fun launchWhenStarted(action: suspend () -> Unit) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            action()
+        }
+    }
+
+    protected open fun showError(message: String) {
+
+    }
+
+    protected open fun showLoading(isLoading: Boolean) {
+
+    }
+}
