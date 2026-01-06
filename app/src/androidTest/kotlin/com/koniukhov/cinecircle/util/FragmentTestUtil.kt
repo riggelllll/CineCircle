256a2ad1c750938c38b22c98cde2563a6c37a1d0
Vladyslav Koniukhov <riggelllll@gmail.com>
2026-01-06 12:55:41 +0200

    Add utility function to launch fragments in Hilt container for testing

---
+++ app/src/androidTest/kotlin/com/koniukhov/cinecircle/util/FragmentTestUtil.kt
@@ -0,0 +1,46 @@
+package com.koniukhov.cinecircle.util
+
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import androidx.annotation.StyleRes
+import androidx.core.util.Preconditions
+import androidx.fragment.app.Fragment
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+
+inline fun <reified T : Fragment> launchFragmentInHiltContainer(
+    fragmentArgs: Bundle? = null,
+    @StyleRes themeResId: Int = 0,
+    crossinline action: Fragment.() -> Unit = {}
+): ActivityScenario<HiltTestActivity> {
+    val startActivityIntent = Intent.makeMainActivity(
+        ComponentName(
+            ApplicationProvider.getApplicationContext(),
+            HiltTestActivity::class.java
+        )
+    ).apply {
+        if (themeResId != 0) {
+            putExtra(
+                "androidx.fragment.app.testing.FragmentScenario.EmptyFragmentActivity.THEME_EXTRAS_BUNDLE_KEY",
+                themeResId
+            )
+        }
+    }
+
+    return ActivityScenario.launch<HiltTestActivity>(startActivityIntent).also { scenario ->
+        scenario.onActivity { activity ->
+            val fragment: Fragment = activity.supportFragmentManager.fragmentFactory.instantiate(
+                Preconditions.checkNotNull(T::class.java.classLoader),
+                T::class.java.name
+            )
+            fragment.arguments = fragmentArgs
+            activity.supportFragmentManager
+                .beginTransaction()
+                .add(android.R.id.content, fragment, "")
+                .commitNow()
+
+            fragment.action()
+        }
+    }
+}
