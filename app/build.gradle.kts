c3c89d355ec0d8dcae1afb05fd033f6c95581446
Vladyslav Koniukhov <riggelllll@gmail.com>
2026-02-01 08:41:30 +0200

    Bump versionCode to 3 in build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.koniukhov.cinecirclex"
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
 
         testInstrumentationRunner = "com.koniukhov.cinecirclex.HiltTestRunner"
